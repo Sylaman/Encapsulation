80ccd03b489f5f290d69f93e957914d301b0f876
Sylaman <Sylaman.snake@gmail.com>
2024-04-06 11:42:55 +0300

    Elevator developed

--- src/Elevator.java
+++ src/Elevator.java
@@ -1,2 +1,41 @@
 public class Elevator {
+
+    int currentFloor = 1;
+    int minFloor;
+    int maxFloor;
+
+    public Elevator(int minFloor, int maxFloor) {
+        this.minFloor = minFloor;
+        this.maxFloor = maxFloor;
+    }
+
+    public void move(int floor) {
+        if (floor > currentFloor) {
+            for (int i = currentFloor; i < floor; i++) {
+                moveUp();
+                System.out.println("Этаж: " + (i + 1));
+            }
+        } else if (floor < currentFloor) {
+            for (int i = currentFloor; i > floor; i--) {
+                moveDown();
+                System.out.println("Этаж: " + (i - 1));
+            }
+        }
+    }
+
+    public void moveDown() {
+        if (currentFloor > minFloor) {
+            currentFloor--;
+        }
+    }
+
+    public void moveUp() {
+        if (currentFloor < maxFloor) {
+            currentFloor++;
+        }
+    }
+
+    public int getCurrentFloor() {
+        return currentFloor;
+    }
 }
